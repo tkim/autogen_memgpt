e2021ebe6b9d2c84f4f22de171f567c622a68968
Terrence Kim <terrence.kim@gmail.com>
2023-11-12 10:03:50 -0500

    create dotenv app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+import autogen
+import os
+import openai
+from memgpt.autogen.memgpt_agent import create_autogen_memgpt_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config_list = [
+    {
+        "model": os.getenv("model")
+        "api_key": os.getenv("api_key")
+    },
+]
+
+openai.api_key = os.getenv("api_key")
+
+llm_config = {
+    "config_list": config_list,
+    "seed": int(os.getenv("seed")),
+    "request_timeout": int(os.getenv("request_timeout"))
+}
+
+# The user agent
+user_proxy = autogen.UserProxyAgent(
+    name="User_proxy",
+    system_mmessage="A human admin.",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={
+        "work_dir": "agenets-workspace",
+        "use_docker": False,
+        
+    }
+)
